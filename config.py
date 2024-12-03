41c6e3f887c1c1ee183855525ce8645766bd7b03
arshy692 <162564228+arshy692@users.noreply.github.com>
2024-12-03 16:48:59 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+TOKEN = "7649804316:AAGk97COXnVFwKwSUwDRZULO8cZjnXNNqcY" # Токен бота
+CRYPTO = "302083:AAxnZcNDjMy7tIpAaPmhDdjhd0FYO6bKPrm" # Токен CryptoBot
+ADMINS = [7944054784, 7550821604] # ID Админов
+
+# Bot Options
+bot_name = "RAVENxBAN"
+bot_logs = "-1002457807107"
+bot_channel_link = "https://t.me/TermRaven"
+bot_admin = "@theMoneybeast1, @ZidxMM, @ClaskMM, @OdiousMM"
+bot_documentation = "https://t.me/+BXreZsDGGHc1M2I1"
+bot_works = "https://t.me/+BXreZsDGGHc1M2I1"
+bot_reviews = "https://t.me/+BXreZsDGGHc1M2I1"
+
+
+# Price
+subscribe_1_day = 2
+subscribe_7_days = 7
+subscribe_14_days = 69.99
+subscribe_30_days = 15
+subscribe_365_days = 69.99
+subscribe_infinity_days = 30ibe_30_days = 15
+subscribe_365_days = 69.99
+subscribe_infinity_days = 30
